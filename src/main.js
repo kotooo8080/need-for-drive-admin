bfd21fc4670c1eeef350092f3570665de28d2050
kotooo8080 <kotooo8080@gmail.com>
2022-04-03 20:19:20 +0300

    V-07 (add auth form for admin panel)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import '@/assets/style/style.scss'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router)
+app.use(VueAxios, axios)
+app.mount('#app');
+
